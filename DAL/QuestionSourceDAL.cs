2b976a71c6f5d96fc02e8fbe2c65332e8d990c1a
Joseph <joseph351619@gmail.com>
2019-04-10 20:33:35 +0800

    Add question source

---
+++ DAL/QuestionSourceDAL.cs
@@ -0,0 +1,19 @@
+﻿using EnglishTester.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishTester.DAL
+{
+    public class QuestionSourceDAL : DALBase<QuestionSource>
+    {
+        public IEnumerable<QuestionSource> SearchQuestion(string words)
+        {
+            Reads();
+            Data = Data.Where(a => a.Source.Contains(words));
+            return Data;
+        }
+    }
+}
